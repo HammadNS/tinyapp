502e1a4a667e63881420660ad329ed3e7ebba702
hammadNS96 <hammad96@outlook.com>
2020-02-17 22:46:51 +0000

    worked on shorturl pt1

--- express_server.js
+++ express_server.js
@@ -8,6 +8,27 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+function generateRandomString(num) {
+  var num = '';
+  var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.length;
+  for ( var i = 0; i < length; i++ ) {
+     num += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return num;
+}
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("200 OK");         // Respond with 'Ok' (we will replace this)
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
